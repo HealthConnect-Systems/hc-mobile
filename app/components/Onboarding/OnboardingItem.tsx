6d55a34c3267abe66f36f5f598ee60c8751ddf71
Kyaw Lin Myat <mr.kyawlinmyat@gmail.com>
2023-08-06 23:37:57 +0630

    feat: Improve onboarding screen.

--- app/components/Onboarding/OnboardingItem.tsx
+++ app/components/Onboarding/OnboardingItem.tsx
@@ -1,13 +1,17 @@
+
+import { Dimensions } from 'react-native';
 import { Image, Title, Text, View } from '../controls';
 import Styles from '../common/Styles';
 
 export default ({ item }: { item: any }) => {
+    const width = Dimensions.get('window').width;
+    const height = Dimensions.get('window').height;
     return (
         <View
             style={{ backgroundColor: item.backgroundColor }}>
             <Title > {item.title} </Title>
             <Image
-                style={Styles.introImageStyle}
+                style={{ width, height, resizeMode: 'contain' }}
                 image={item.image} />
             <Text style={Styles.introTextStyle}>
                 {item.text}
--- app/components/Onboarding/index.tsx
+++ app/components/Onboarding/index.tsx
@@ -1,22 +1,32 @@
 import React from 'react';
-import { NextButton, DoneButton, PrevButton, SkipButton } from '../controls';
+import { useColorScheme } from 'react-native';
 import OnboardingItem from './OnboardingItem';
 import Slides from './Slides';
 import AppIntroSlider from 'react-native-app-intro-slider';
+import Themed from '../common/Themed';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 type Props = {
     onDone: () => void;
 }
 export default (props: Props) => {
+    const theme = useColorScheme() === 'dark' ? Themed.dark : Themed.light;
+    const insets = useSafeAreaInsets();
     return (
-        <AppIntroSlider
-            data={Slides}
-            renderItem={OnboardingItem}
-            onDone={props.onDone}
-            showPrevButton={true}
-            renderDoneButton={() => <DoneButton />}
-            renderPrevButton={() => <PrevButton />}
-            renderNextButton={() => <NextButton />}
-        />
+        <>
+            <AppIntroSlider
+                data={Slides}
+                renderItem={OnboardingItem}
+                style={{
+                    backgroundColor: theme.colors.background,
+                    shadowColor: theme.colors.background,
+                    alignContent: 'center'
+                }}
+                onDone={props.onDone}
+                showPrevButton={true}
+                showSkipButton={true}
+                contentInset={insets}
+            />
+        </>
     );
 }; 
